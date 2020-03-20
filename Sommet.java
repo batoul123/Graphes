cb7d2ffc0c8dbe11cdc5c465b9e64cfc70d2a296
batoul123 <58598912+batoul123@users.noreply.github.com>
2020-03-20 22:37:44 +0100

    Add files via upload

--- Sommet.java
+++ Sommet.java
@@ -1,5 +1,15 @@
-package graphes;
 
 public class Sommet {
+private int NumSomet;
+public Sommet(int NumSomet){
+	this.NumSomet=NumSomet;
+}
 
+public int getNumSomet() {
+	return NumSomet;
+}
+
+public void setNumSomet(int numSomet) {
+	NumSomet = numSomet;
+}
 }
