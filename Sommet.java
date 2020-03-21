4b35f44f196f639ede999d1741802cb9319c3323
Insafsalma <58422379+Insafsalma@users.noreply.github.com>
2020-03-21 18:30:17 +0100

    Update Sommet.java

--- Sommet.java
+++ Sommet.java
@@ -1,5 +1,16 @@
 package graphes;
 
 public class Sommet {
+private int NumSomet;
+public Sommet(int NumSomet){
+	this.NumSomet=NumSomet;
+}
 
+public int getNumSomet() {
+	return NumSomet;
+}
+
+public void setNumSomet(int numSomet) {
+	NumSomet = numSomet;
+}
 }
